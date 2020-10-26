5e67cd6a71f37e0baec0fc9ba53aa3cd937e2b60
vseredov <vasily.seredov@capgemini.com>
2020-10-26 11:57:06 +0100

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -9,6 +9,17 @@ public class BestLoanRates {
         System.out.println ("Enter your name");
         String name = scanner.nextLine ();
         System.out.println ("Hello " + name);
+        System.out.println ("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt ();
+        float bestRate = getRates ( loanTermInYears );
+
+        if(bestRate == 0.0f) {
+            System.out.println ("No available rates for term: " + loanTermInYears + " years");
+        }
+        else {
+            System.out.println ("Best Available Rate: " + getRates(loanTermInYears) + "%");
+        }
+        scanner.close ();
     }
     public static final Map<Integer, Float> bestRates = Map.of (1, 5.50f, 2, 3.45f, 3, 2.67f);
 
