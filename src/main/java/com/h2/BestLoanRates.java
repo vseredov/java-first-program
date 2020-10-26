cb1dcb885c6a2bf2ef7e322d01e4d33996e294c3
vseredov <vasily.seredov@capgemini.com>
2020-10-26 11:38:32 +0100

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,8 +1,13 @@
 package com.h2;
 
+import java.util.Scanner;
+
 public class BestLoanRates {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner ( System.in );
+        System.out.println ("Enter your name");
+        String name = scanner.nextLine ();
+        System.out.println ("Hello " + name);
     }
 
 }
