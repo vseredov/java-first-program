29d9b875487384a50f9933fd3638c7a35ff1de0d
vseredov <vasily.seredov@capgemini.com>
2020-10-27 11:20:03 +0100

    Completed module

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -32,7 +32,7 @@ public class SavingsCalculator {
     private static int remainingDaysInMonth(LocalDate date) {
         YearMonth yearMonth = YearMonth.of ( date.getYear (), date.getMonth () );
         int totalDaysInMonth = yearMonth.lengthOfMonth ();
-        int remainingDays = date.getDayOfMonth ();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth ();
         return remainingDays;
     }
 }
