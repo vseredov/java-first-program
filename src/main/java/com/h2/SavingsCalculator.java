5d62f0a28f9fe059ea68e9bd6352102cccccd420
vseredov <vasily.seredov@capgemini.com>
2020-10-27 12:16:56 +0100

    Completed module

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -38,7 +38,7 @@ public class SavingsCalculator {
     }
 
     public float calculate() {
-        return  (sumOfCredits() - sumOfDebits ());
+        return  -(sumOfCredits() - sumOfDebits ());
     }
 
     public static void main(String args[]) {
