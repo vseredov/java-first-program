5eb16b63e36dc6beb92f3a263d56f872c2969271
vseredov <vasily.seredov@capgemini.com>
2020-10-28 14:07:56 +0100

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -2,5 +2,8 @@
 
 
 public class Finance {
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR  = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR  = "mortgageCalculator";
 
 }
