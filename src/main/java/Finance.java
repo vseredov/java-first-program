1db1baedec9431807e0e745ffad8fc0d582b7418
vseredov <vasily.seredov@capgemini.com>
2020-10-28 14:17:30 +0100

    Completed module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -12,6 +12,14 @@ public class Finance {
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
     private static boolean validateCommandArguments(String [] args) {
+        switch (args[0]) {
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
+        }
         return false;
     }
 }
