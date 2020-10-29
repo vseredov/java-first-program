ba92ab1dbba6f6aa55672349410df88cabc84cb3
vseredov <vasily.seredov@capgemini.com>
2020-10-29 11:56:57 +0100

    Completed module

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -6,16 +6,34 @@ public class Utilities {
         long out = Long.MIN_VALUE;
         try {
             out = Long.parseLong ( in );
-
         }
         catch (NumberFormatException e) {
             throw new IllegalArgumentException ( in + " cannot be converted into a 'long' value. Exiting program." );
         }
-
-
         return out;
     }
 
+    public static int getIntValue(String in) {
+        int out = Integer.MIN_VALUE;
+        try {
+            out = Integer.parseInt ( in );
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException ( in + " cannot be converted into a 'int' value. Exiting program." );
+        }
+        return out;
+    }
+
+    public static float getFloatValue(String in) {
+        float out = Float.MIN_VALUE;
+        try {
+            out = Float.parseFloat ( in );
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException ( in + " cannot be converted into a 'float' value. Exiting program." );
+        }
+        return out;
+    }
 
 
 }
